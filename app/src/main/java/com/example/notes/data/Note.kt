5fff8c9792088e1e620948a3d50d6f90dae7c986
vktryy <reneva_v@mail.ru>
2025-05-07 16:06:54 +0300

    папки

---
+++ app/src/main/java/com/example/notes/data/Note.kt
@@ -0,0 +1,8 @@
+package com.example.notes.data
+
+data class Note(
+    val id: String,
+    val title: String,
+    val content: String,
+    val date: Long
+)
---
+++ app/src/main/java/com/example/notes/utils/Retrofit.kt
@@ -0,0 +1,15 @@
+package com.example.notes.utils
+
+import retrofit2.Retrofit
+
+object RetrofitClient {
+    private const val BASE_URL = "https://your-api.com/"
+
+//    val instance: NotesApi by lazy {
+//        Retrofit.Builder()
+//            .baseUrl(BASE_URL)
+//            .addConverterFactory(GsonConverterFactory.create())
+//            .build()
+//            .create(NotesApi::class.java)
+//    }
+}
